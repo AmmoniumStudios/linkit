1561f5b4e4bfbfc49c274ec2df43fa235f24b89c
Alex O'Brien <alex8obrien@gmail.com>
2024-01-14 22:54:39 +0000

    Added short code generator

--- cli/app/src/main/java/org/ammonium/linkit/cli/Add.java
+++ cli/app/src/main/java/org/ammonium/linkit/cli/Add.java
@@ -1,5 +1,6 @@
 package org.ammonium.linkit.cli;
 
+import org.ammonium.linkit.util.CodeGenerator;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Parameters;
@@ -7,28 +8,37 @@ import picocli.CommandLine.Parameters;
 import java.util.concurrent.Callable;
 
 @Command(
-        name = "add",
-        description = "Add command"
+    name = "add",
+    description = "Add command"
 )
 public class Add implements Callable<Integer> {
 
     @CommandLine.Option(
-            names = { "-s", "--short" },
-            description = "The short link for the database",
-            defaultValue = "TODO" // TODO: Code Generator
+        names = {"-s", "--short"},
+        description = "The short link for the database"
     )
     private String shortLink;
 
     @Parameters(
-            paramLabel = "url",
-            description = "The full URL of the website",
-            arity = "1"
+        paramLabel = "url",
+        description = "The full URL of the website",
+        arity = "1"
     )
     private String url;
 
     @Override
     public Integer call() {
-        System.out.println("Add command coming soon");
+
+        if (shortLink == null) {
+            shortLink = CodeGenerator.generateCode();
+        }
+
+        // TODO: Create the json request
+
+        // TODO: Send request
+
+        // TODO: Handle response
+
         return 0;
     }
 }
