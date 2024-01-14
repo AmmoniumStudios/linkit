aee040f45070abb1f8566657049fa69c9581cedb
Aren Marsden <aren.marsden6@gmail.com>
2024-01-14 17:02:42 +0000

    Cleanup main

--- cli/app/src/main/java/org/ammonium/linkit/Application.java
+++ cli/app/src/main/java/org/ammonium/linkit/Application.java
@@ -1,16 +1,8 @@
 package org.ammonium.linkit;
 
-import org.ammonium.linkit.util.HttpUtil;
-import org.ammonium.linkit.util.json.Body;
-
-public class Application {
-    public String getGreeting() {
-        return "Hello World!";
-    }
+public final class Application {
 
     public static void main(String[] args) {
-        //System.out.println(new Gson().toJson(new Body("SELECT * FROM `links` WHERE `short` = ?", new String[]{"google"})));
-        String url = HttpUtil.shortenUrl(new Body("SELECT * FROM `links` WHERE `short` = ?", new String[]{"google"}));
-        System.out.println(url);
+
     }
 }
