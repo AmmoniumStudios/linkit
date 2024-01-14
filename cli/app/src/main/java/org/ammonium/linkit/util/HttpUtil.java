51a863983628c0997e7af78d82e215b059b7a6e4
Aren Marsden <aren.marsden6@gmail.com>
2024-01-14 02:19:14 +0000

    Add method shortenUrl
    Signed-off-by: Aren Marsden <aren.marsden6@gmail.com>

--- cli/app/src/main/java/org/ammonium/linkit/util/HttpUtil.java
+++ cli/app/src/main/java/org/ammonium/linkit/util/HttpUtil.java
@@ -1,6 +1,8 @@
 package org.ammonium.linkit.util;
 
+import okhttp3.Headers;
 import okhttp3.MediaType;
+import okhttp3.Request;
 import okhttp3.RequestBody;
 import org.ammonium.linkit.model.http.ShortUrl;
 
@@ -14,9 +16,18 @@ public final class HttpUtil {
         .replace("{database_identifier}", System.getenv("CLOUDFLARE_DATABASE_IDENTIFIER"));
 
     private static final MediaType APPLICATION_JSON = MediaType.get("application/json");
+    private static final Headers HEADERS = Headers.of(
+        "Authorization", "Bearer %s".formatted(System.getenv("CLOUDFLARE_BEARER_TOKEN"))
+    );
 
     public static ShortUrl shortenUrl(String json) {
         final RequestBody body = RequestBody.create(json, APPLICATION_JSON);
+        final Request request = new Request.Builder()
+            .url(CLOUDFLARE_API)
+            .headers(HEADERS)
+            .post(body)
+            .build();
+
         return null;
     }
 
