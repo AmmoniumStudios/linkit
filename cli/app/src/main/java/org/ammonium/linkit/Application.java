835997cab5b9ab4b7b4c845b736ca8311472c1c3
Alex O'Brien <alex8obrien@gmail.com>
2024-01-15 00:03:29 +0000

    Implemented Main method

--- cli/app/src/main/java/org/ammonium/linkit/Application.java
+++ cli/app/src/main/java/org/ammonium/linkit/Application.java
@@ -1,8 +1,12 @@
 package org.ammonium.linkit;
 
+import org.ammonium.linkit.cli.Linkit;
+import picocli.CommandLine;
+
 public final class Application {
 
     public static void main(String[] args) {
 
+        new CommandLine(new Linkit()).execute(args);
     }
 }
